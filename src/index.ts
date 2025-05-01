a4c31d3106dfd6f31ef81df173c5df9e805ddace
michael <michaelslack42@gmail.com>
2025-05-01 22:24:12 +0100

    Update exports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import {Hasse} from "./Hasse/Hasse"
-import {HasseNode} from "./Hasse/HasseNode"
-import {PartialOrdering} from "./Common/PartialOrdering"
-import {Relation} from "./Common/Relation"
-import {Hasseoid} from "./Hasseoid/Hasseoid"
-import {HasseoidNode} from "./Hasseoid/HasseoidNode"
+import { Hasse } from "./Hasse/Hasse"
+import { HasseNode } from "./Hasse/HasseNode"
+import { PartialOrdering } from "./Common/PartialOrdering"
+import { Relation } from "./Common/Relation"
+import { Hasseoid } from "./Hasseoid/Hasseoid"
+import { HasseoidNode } from "./Hasseoid/HasseoidNode"
+import { OrderingFn, PartialOrderingFn, TotalOrderingFn } from "./Common/OrderingFns"
 
-export {Hasse, HasseNode, Hasseoid, HasseoidNode, PartialOrdering, Relation}
+export { Hasse, HasseNode, Hasseoid, HasseoidNode, PartialOrdering, PartialOrderingFn, TotalOrderingFn, OrderingFn, Relation }
